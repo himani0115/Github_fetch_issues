2c605da7d4d576c1e77518401f698637fc03609f
himanijoshi0191 <joshihimani0191@gmail.com>
2021-02-12 18:55:36 +0530

    convert json data to .csv file

--- git_issues/views.py
+++ git_issues/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render
 import requests
 import json
+import pandas as pd
 # Create your views here.
+
 
 def home(request):
     return render(request, 'home.html')
 
 def github_issues(request):
     if request.method == "POST":
-        token= request.POST['auth_token']
+        token = request.POST['auth_token']
         owner = request.POST['username']
-        repo=request.POST['repo_name']
+        repo = request.POST['repo_name']
+        file_name = request.POST['file_name']
     query_url = f"https://api.github.com/repos/{owner}/{repo}/issues"
     params = {
         "state": "open",
-        }
+    }
     headers = {'Authorization': f'{token}'}
 
     responce = requests.get(query_url, headers=headers, params=params)
-    data= responce.json()
+    data = responce.json()
+    df = pd.DataFrame.from_dict(data)
+    if file_name:
+        df.to_csv(file_name+'.csv')
     context = {
         'data': data,
     }
-    return render(request,'github_issues.html',context)
+    return render(request, 'github_issues.html', context)
